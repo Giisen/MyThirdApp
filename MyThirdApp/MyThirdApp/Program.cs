48ae789720ef8454f7dafa667af014e8b3fc67ef
giisen <kristerekelund@me.com>
2022-08-29 14:48:38 +0200

    new

--- MyThirdApp/MyThirdApp/Program.cs
+++ MyThirdApp/MyThirdApp/Program.cs
@@ -1,2 +1,23 @@
 ﻿
+// deklarera vårt namn,ålder och en bool om vi har ätit frukost. Skriv ut dessa i WriteLine
+
+
+
+var namn = "Krister";
+var age = 45;
+bool frukost = true;
+
+
 Console.WriteLine("Hello Krister Ekelund!");
+
+int heltal;
+
+heltal = 1;
+
+var heltal2 = 2L;
+
+Console.WriteLine($"Heltal: {heltal}");
+Console.WriteLine("Heltal: " + heltal);
+Console.WriteLine($"Hej {namn} din ålder är {age} status för din frukost är {frukost} ");
+
+
